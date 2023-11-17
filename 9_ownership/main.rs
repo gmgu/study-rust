2129148590026328b86800de738909e9ab68361a
gmgu <gmgu@theory.snu.ac.kr>
2023-11-17 18:38:58 +0900

    add 9_ownership

---
+++ 9_ownership/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+  {
+    let s = "hello";  // s is valid from here
+  }  // s is no longer valid
+  //println!("{}", s);  // s is not valid here
+
+  let x = 5;  // x is stored in the stack
+  let y = x;  // y is stored in the stack
+  println!("x: {}, y: {}", x, y);  // no ownership changes
+
+  let a = String::from("hello");  // a is pointer. data is stored in the heap
+  println!("a: {}", a);
+  let b = a;  // b is a pointer. ownership of the data is passed to a from b; a is no more valid
+  println!("b: {}", b);
+  // println!("a: {}", a); // a is not valid
+
+  let a = String::from("world");
+  let b = a.clone();  // copied
+  println!("a: {}, b: {}", a, b);
+
+  take_ownership(a);
+  //println!("a: {}", a);  // a is not valid
+
+  let mut a = give_ownership();
+  println!("a: {}", a);
+
+  a = take_and_return_ownership(a);
+  println!("a: {}", a);
+}
+
+fn take_ownership(a: String) -> (){
+}
+
+fn give_ownership() -> String {
+  let a = String::from("hi");
+  return a;
+}
+
+fn take_and_return_ownership(mut a: String) -> String {
+  a.push_str(" there");
+  return a;
+}
